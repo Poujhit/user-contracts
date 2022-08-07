6216fb673ed0c6f22bc781c3b07a7f5aeb1dd36e
Poujhit <poujhit.dev@gmail.com>
2022-08-07 22:13:54 +0530

    Initialsed chakra UI

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>User Contracts</title>
+        <meta
+          name='description'
+          content='Trying to implement C2C transactions using smart contracts'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
