6dac197013b3248209856ef059223b83a60898a1
Poujhit <poujhit.dev@gmail.com>
2022-08-07 22:13:12 +0530

    C2C contract created

---
+++ contracts/C2cContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+/// @title C2C contract
+/// @author Poujhit
+/// @notice A C2C contract between a buyer and seller
+/// @dev None as of now
+
+contract C2cContract {
+
+}
