e4c1881f4f2100296a9bb7442971a8541f81a5b0
Poujhit <poujhit.dev@gmail.com>
2022-08-08 23:32:22 +0530

    C2C contract basics finished

--- contracts/C2cContract.sol
+++ contracts/C2cContract.sol
@@ -7,8 +7,81 @@ import "hardhat/console.sol";
 /// @title C2C contract
 /// @author Poujhit
 /// @notice A C2C contract between a buyer and seller
-/// @dev None as of now
+/// @dev Buyer is the one creating this contract and will send the money to the seller. While deploying the contract,
+/// buyer will send the price amount in msg.value. Now the contract will hold the amount. Later the seller can withdraw
+/// the funds using the withdrawFunds function. Only seller can withdraw the funds.
 
 contract C2cContract {
+    address payable private buyer;
+    address payable private seller;
+    uint256 private price;
+    uint64 private startDate;
+    uint64 private endDate;
 
+    bool private withdrawed;
+
+    error Unauthorized();
+    error Withdrawed();
+
+    constructor(
+        address payable sellerAddress,
+        uint256 priceOfGoods,
+        uint64 date1,
+        uint64 date2
+    ) payable {
+        seller = sellerAddress;
+        // when deploying the contract, owner is the buyer and msg.sender will have the owner address only
+        buyer = payable(msg.sender);
+        price = priceOfGoods;
+        startDate = date1;
+        endDate = date2;
+        withdrawed = false;
+    }
+
+    function getBuyer()
+        public
+        view
+        onlySellerOrBuyer
+        returns (address payable)
+    {
+        return buyer;
+    }
+
+    function getSeller() public view onlySellerOrBuyer returns (address) {
+        return seller;
+    }
+
+    function getPrice() public view onlySellerOrBuyer returns (uint256) {
+        return price;
+    }
+
+    function getStartDate() public view onlySellerOrBuyer returns (uint64) {
+        return startDate;
+    }
+
+    function getEndDate() public view onlySellerOrBuyer returns (uint64) {
+        return endDate;
+    }
+
+    function withdrawFunds() public payable notYetWithdrawed onlySeller {
+        console.log(msg.sender);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+        withdrawed = true;
+    }
+
+    modifier onlySellerOrBuyer() {
+        if (msg.sender != seller && msg.sender != buyer) revert Unauthorized();
+        _;
+    }
+    modifier onlySeller() {
+        if (msg.sender != seller) revert Unauthorized();
+        _;
+    }
+    modifier notYetWithdrawed() {
+        if (withdrawed) revert Withdrawed();
+        _;
+    }
 }
