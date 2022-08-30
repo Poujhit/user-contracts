7e58d9b4be0ff060ab84bd651052cacc212c496f
Poujhit <poujhit.dev@gmail.com>
2022-08-30 21:42:56 +0530

    Integrated buy product route except buy

--- frontend/pages/buy-product.tsx
+++ frontend/pages/buy-product.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useState } from 'react';
 
@@ -12,27 +12,58 @@ import {
   InputLeftAddon,
   Text,
 } from '@chakra-ui/react';
+import axios from 'axios';
 
 interface IndexProps {}
 
-const HomePage: NextPage<IndexProps> = () => {
+const BuyProduct: NextPage<IndexProps> = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const response = await axios.get('/api/getProducts');
+      setData(response.data?.data ?? []);
+      setLoading(false);
+    }
+    getData();
+  }, []);
+
   return (
-    <Flex
-      height='100vh'
-      alignItems='center'
-      justifyContent='center'
-      direction='column'
-    >
-      <Flex
-        direction='column'
-        background='gray.100'
-        p={12}
-        rounded={6}
-        alignItems='center'
-      >
-        <Heading mb={6}>Buy products (Create a contract)</Heading>
-      </Flex>
+    <Flex alignItems='center' justifyContent='start' direction='column'>
+      <Heading mb={6} position='fixed'>
+        Buy products (Create a contract)
+      </Heading>
+      {loading ? (
+        <Text mt={40}>Loading...</Text>
+      ) : (
+        data.map((prod, index) => (
+          <Flex
+            key={index}
+            direction='column'
+            background='gray.100'
+            p={12}
+            mt={16}
+            rounded={6}
+            alignItems='center'
+          >
+            <pre>{JSON.stringify(prod)}</pre>
+            <Button
+              color='white'
+              colorScheme='teal'
+              mt={6}
+              bgGradient={'linear(to-r, teal.500, green.500)'}
+              onClick={() => {
+                // router.push('/see-contracts');
+              }}
+            >
+              Buy
+            </Button>
+          </Flex>
+        ))
+      )}
     </Flex>
   );
 };
-export default HomePage;
+export default BuyProduct;
