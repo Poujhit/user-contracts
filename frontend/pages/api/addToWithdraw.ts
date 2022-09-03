aa9af1197863451b0c25c6bd035116247374d456
Poujhit <poujhit.dev@gmail.com>
2022-09-03 23:16:55 +0530

    Important apis added

--- frontend/pages/api/addToWithdraw.ts
+++ frontend/pages/api/addToWithdraw.ts
@@ -1,4 +1,7 @@
-// add it in orders collection, document key is the phone number of the user.
+// withdraw collection will have all the data that a buyer has brought your product and it's data is stored in the contract
+// and the seller can see that transaction. using this data he can withdraw the amount that the buyer has put in that
+// contract
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createClient } from 'redis';
 
@@ -16,19 +19,20 @@ export default async function handler(
 
   await db.connect();
 
-  const result = (await db.json.get('orders')) as Record<string, any>;
+  const result = (await db.json.get('withdraw')) as Record<string, any>;
   const isOldUser = Object.keys(result).includes(req.body.phone);
   if (!isOldUser) {
-    await db.json.set('orders', req.body.phone, [
+    await db.json.set('withdraw', req.body.phone, [
       {
         contractAddress: req.body.contractAddress,
       },
     ]);
   } else {
-    await db.json.arrAppend('orders', `${req.body.phone}`, {
+    await db.json.arrAppend('withdraw', `${req.body.phone}`, {
       contractAddress: req.body.contractAddress,
     });
   }
   await db.disconnect();
+
   res.status(200).json({ success: true });
 }
