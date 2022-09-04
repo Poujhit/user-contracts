aa964d040ab1a56db1d27d630f2b196b099319db
Poujhit <poujhit.dev@gmail.com>
2022-09-04 23:20:40 +0530

    get orders and withdraws api added

---
+++ frontend/pages/api/getMyWithdraw.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createClient } from 'redis';
+
+type Data = {
+  success: boolean;
+  orders?: Record<string, any>[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const db = createClient({
+    url: process.env.REDIS_URL,
+  });
+
+  await db.connect();
+
+  const result = (await db.json.get('withdraw')) as Record<string, any>;
+  const isOldUser = Object.keys(result).includes(req.body.phone);
+
+  if (!isOldUser) {
+    res.status(200).json({ success: true, orders: [] });
+  } else {
+    res.status(200).json({ success: true, orders: result[req.body.phone] });
+  }
+  await db.disconnect();
+}
